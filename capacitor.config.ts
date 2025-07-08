d0cc1886bfdce00332c884abd71ee1d00f0f6621
Abrahamart21 <abraham213art@gmail.com>
2025-07-07 21:44:22 -0500

    Primer paso

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'XO_music',
+  webDir: 'www'
+};
+
+export default config;
